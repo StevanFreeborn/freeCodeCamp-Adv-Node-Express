4b42ce1a2ff2b6bb977d2dc6a621531ef8316dfa
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-10 14:56:38 -0500

    add logout and handle missing pages

--- server.js
+++ server.js
@@ -84,6 +84,17 @@ myDB(async client => {
     res.render('pug/profile', { username: req.user.username });
   });
 
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
 }).catch(err => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: err, message: 'Unable to login' });
