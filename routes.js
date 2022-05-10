1c7a4aa5e586070b127a59a2bd7845f06f1644e6
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-10 16:14:29 -0500

    add github auth and callback routes

--- routes.js
+++ routes.js
@@ -18,7 +18,8 @@ module.exports = function (app, myDataBase) {
             title: 'Connected to Database',
             message: 'Please login',
             showLogin: true,
-            showRegistration: true
+            showRegistration: true,
+            showSocialAuth: true
         });
     });
 
@@ -85,6 +86,12 @@ module.exports = function (app, myDataBase) {
             res.redirect('/profile');
         }
     );
+
+    app.route('/auth/github').get(passport.authenticate('github'));
+
+    app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile');
+    });
     
     app.use((req, res, next) => {
         res.status(404)
